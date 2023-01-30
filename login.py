89ce4998f753626b974841773b038b07bc34d0d9
Laboratorio <Laboratorio@unida.edu.py>
2023-01-30 16:09:44 -0300

    clase final

--- login.py
+++ login.py
@@ -2,11 +2,34 @@ from flask import Blueprint, request, jsonify
 login = Blueprint('/login',__name__)
 
 @login.route('/login', methods=['POST'])
-def llamarServicios():
-    global usar,passw
+def llamarServicioSet():
+    global user,password
 
     user =request.json['user']
-    passw =request.json['passw']
+    password =request.json['password']
+    inicializarVariables(user,password)
 
-    print("username",user)
-    print("password",passw)
+
+    salida = {'codRes': codRes, 'menRes': menRes, 'usuario':user, 'accion': accion}
+    return jsonify(salida)
+
+
+def inicializarVariables(user,password):
+    global codRes, menRes,accion
+    userLocal= "marcos"
+    passLocal="123"
+    codRes ='SIN_ERROR'
+    menRes='OK'
+    try:     
+        print("verificar login")
+        if(str(password)==str(passLocal) and str(user)==str(userLocal)):
+            print("Usuario y contraseña OK")
+            accion="succes"
+        else:
+            print("Usuario o contraseña incorrecta")
+            accion="Usuario o contraseña incorrecta"
+
+    except Exception as e:
+        print("Error",str(e))
+        codRes='Error'
+        menRes='Msg'+ str (e)
